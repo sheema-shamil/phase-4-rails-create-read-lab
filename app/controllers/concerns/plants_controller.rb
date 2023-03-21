5851a6d54d62e587f831412ff6fff954a0e47b10
sheema <sheemashamilfarook@gmail.com>
2023-03-21 12:59:09 +0300

    done

---
+++ app/controllers/concerns/plants_controller.rb
@@ -0,0 +1,26 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant, status: 200
+        else
+            render json: {error: "Plant not found"}, status: 404
+        end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: 201
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
